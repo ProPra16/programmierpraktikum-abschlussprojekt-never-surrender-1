e6558da984288420e16b4d12e111207d06f8b49e
Felix Oltersdorf <f-oltersdorf@web.de>
2016-06-25 17:27:20 +0200

    Loader erstellt und implementiert

---
+++ src/tde/file/Loader.java
@@ -0,0 +1,22 @@
+package tde.file;
+
+import java.io.BufferedReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Loader {
+    public String[] readFromFile(Path filePath){
+        Charset charset = Charset.forName("US-ASCII");
+        List<String> lines = new ArrayList<>();
+        try(BufferedReader reader = Files.newBufferedReader(filePath, charset)){
+            String line;
+            while((line = reader.readLine()) != null)
+                lines.add(line);
+            reader.close();
+        } catch (Exception ex) {}
+        return lines.toArray(new String[lines.size()]);
+    }
+}
