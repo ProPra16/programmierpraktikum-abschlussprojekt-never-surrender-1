56b9937fbb678e94463682558f09b2ac17044c6f
Felix Oltersdorf <f-oltersdorf@web.de>
2016-06-25 17:04:28 +0200

    TestDrivenEnviroment erstellt

---
+++ src/tde/core/TestDrivenEnviroment.java
@@ -0,0 +1,14 @@
+package tde.core;
+
+import javafx.stage.Window;
+
+/**
+ * Hauptklasse der ganzen Anwendung
+ * lädt die fxml-Datei für die gui
+ */
+
+public class TestDrivenEnviroment extends Window {
+    public TestDrivenEnviroment(){
+
+    }
+}
