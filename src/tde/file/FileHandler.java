98358c9e217a0f93646d8783392467f682537575
Felix Oltersdorf <f-oltersdorf@web.de>
2016-06-25 19:05:47 +0200

    FileHandler ergänzt um leere Funtionen: addFile, deleteFile, write, read

--- src/tde/file/FileHandler.java
+++ src/tde/file/FileHandler.java
@@ -8,4 +8,20 @@ public class FileHandler {
     public FileHandler(Path rootDirectory){
         this.rootDirectory = rootDirectory;
     }
+
+    public void addFile(String fileName){
+
+    }
+
+    public void deleteFile(String fileName){
+
+    }
+
+    public void write(String fileName, String[] lines){
+
+    }
+
+    public String[] read(String fileName){
+        return new String[0];
+    }
 }
