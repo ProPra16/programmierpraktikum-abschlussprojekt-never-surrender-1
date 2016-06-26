9d5d36dde670fa50ee299f3c21d09859d55b704b
Felix Oltersdorf <f-oltersdorf@web.de>
2016-06-26 12:32:29 +0200

    Saver dokumentiert

--- src/tde/file/Saver.java
+++ src/tde/file/Saver.java
@@ -5,8 +5,17 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Saver {
-    public void saveToFile(String[] lines, Path filePath){
+/**
+ * Eine Klasse zum Schreiben von Dateien
+ */
+
+class Saver {
+    /**
+     * Schreibt Text in eine Datei
+     * @param lines Die zu schreibenen Zeilen
+     * @param filePath Dateipfad
+     */
+    void saveToFile(String[] lines, Path filePath){
         Charset charset = Charset.forName("US-ASCII");
         try(BufferedWriter writer = Files.newBufferedWriter(filePath, charset)) {
             for(String s: lines){
