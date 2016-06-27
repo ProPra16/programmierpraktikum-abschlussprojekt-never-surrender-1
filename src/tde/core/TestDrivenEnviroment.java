d08ac86cce6adbcaa4e395c40cd01bc73fc6f4d3
Felix Oltersdorf <felixoltersdorf@Felixs-MacBook-Air-2.local>
2016-06-27 11:17:21 +0200

    testDrivenDevelopment kommentiert

--- src/tde/core/TestDrivenEnviroment.java
+++ src/tde/core/TestDrivenEnviroment.java
@@ -14,6 +14,7 @@ import javafx.stage.Stage;
 public class TestDrivenEnviroment extends Application {
 	@Override
     public void start(Stage primaryStage) throws Exception{
+		//Hier wird die GUI geladen
     	Parent root = new GridPane();
     	primaryStage.setTitle("TestDrivenEnviroment");
     	primaryStage.setScene(new Scene(root));
