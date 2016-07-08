27451604be0435288e13c83b9b81ca821ad4fe96
ErichWitlinger <erich.witlinger@uni-duesseldorf.de>
2016-07-08 16:50:51 +0200

    Sachen bereinigt

--- src/main/java/tde/file/fxmlParser.java
+++ src/main/java/tde/file/fxmlParser.java
@@ -1,13 +1,33 @@
 package tde.file;
 
+import java.nio.file.Path;
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+
 public class fxmlParser {
 
-    public static void codeToData(String code, String location) {
+    public void codeToData(String code, String location, String name) {
 
     }
 
-    public static String dataToCode(String location) {
-        String code = new String();
-        return code;
+    public String dataToCode(Path filePath) {
+        try {
+             File file = new File("exercises.txt");
+
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        String xmlCode = new String();
+        //wie kann ich readFromFile aus Loader.class ansprechen?
+        //xmlCode.readFromFile(filePath);
+
+        String Code = new String();
+        return Code;
     }
 }
--- src/main/java/tde/gui/fesf/7/src/test.java
+++
@@ -1,7 +0,0 @@
-import vk.core.api.CompilationUnit;
-
-public class test {
-	
-	public CompilationUnit[] CUar = new CompilationUnit[];
-	
-}
