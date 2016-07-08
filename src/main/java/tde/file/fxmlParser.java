976e6d9dd18107faaffdfe622c233d5941369fed
ErichWitlinger <erich.witlinger@uni-duesseldorf.de>
2016-07-08 15:30:17 +0200

    Die beiden Methoden zum parsen erstellt

--- src/main/java/tde/file/fxmlParser.java
+++ src/main/java/tde/file/fxmlParser.java
@@ -1,4 +1,13 @@
 package tde.file;
 
 public class fxmlParser {
+
+    public static void codeToData(String code, String location) {
+
+    }
+
+    public static String dataToCode(String location) {
+        String code = new String();
+        return code;
+    }
 }
