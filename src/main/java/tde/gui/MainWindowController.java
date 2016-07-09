f4c25f16a156f752e81dab8cbaa5698e416fd4dc
Michael Borisov <michaelborisov9@gmail.com>
2016-07-09 20:38:46 +0200

    Controller TextArea wird grau und nicht editierbar

--- src/main/java/tde/gui/MainWindowController.java
+++ src/main/java/tde/gui/MainWindowController.java
@@ -2,6 +2,8 @@ package tde.gui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Region;
 import tde.core.TestDrivenEnviroment;
 
 /**
@@ -9,7 +11,26 @@ import tde.core.TestDrivenEnviroment;
  */
 
 public class MainWindowController {
-	@FXML protected void handleLoadButtonAction(ActionEvent event) {
-
+	@FXML TextArea text1;
+	@FXML TextArea text2;
+	
+	@FXML protected void testCode(ActionEvent event) {
+		Region region1 = (Region) text1.lookup(".content");
+		Region region2 = (Region) text2.lookup(".content");
+		
+		region1.setStyle("-fx-background-color: gray");
+		text1.setDisable(true);
+		region2.setStyle("-fx-background-color: white");
+		text2.setDisable(false);
+	}
+	
+	@FXML protected void checkTest(ActionEvent event) {
+		Region region1 = (Region) text1.lookup(".content");
+		Region region2 = (Region) text2.lookup(".content");
+		
+		region2.setStyle("-fx-background-color: gray");
+		text2.setDisable(true);
+		region1.setStyle("-fx-background-color: white");
+		text1.setDisable(false);
 	}
 }
