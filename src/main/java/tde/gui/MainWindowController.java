281f3654ae5ee62f3feef57cc61162b200b1aaf7
Michael Borisov <michaelborisov9@gmail.com>
2016-07-09 20:41:13 +0200

    Import deleted

--- src/main/java/tde/gui/MainWindowController.java
+++ src/main/java/tde/gui/MainWindowController.java
@@ -4,7 +4,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
-import tde.core.TestDrivenEnviroment;
 
 /**
  * Controller für die eigentliche Benutzeroberfläche
