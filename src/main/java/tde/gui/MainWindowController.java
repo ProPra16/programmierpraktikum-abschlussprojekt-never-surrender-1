15db7a31614bd303a1adf333184f969cf7a84d9a
Michael Borisov <michaelborisov9@gmail.com>
2016-07-09 20:47:44 +0200

    Controller updated

--- src/main/java/tde/gui/MainWindowController.java
+++ src/main/java/tde/gui/MainWindowController.java
@@ -4,32 +4,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
+import javafx.scene.web.HTMLEditor;
 
 /**
  * Controller für die eigentliche Benutzeroberfläche
  */
 
 public class MainWindowController {
-	@FXML TextArea text1;
-	@FXML TextArea text2;
+	@FXML HTMLEditor text1;
+	@FXML HTMLEditor text2;
 	
 	@FXML protected void testCode(ActionEvent event) {
-		Region region1 = (Region) text1.lookup(".content");
-		Region region2 = (Region) text2.lookup(".content");
-		
-		region1.setStyle("-fx-background-color: gray");
 		text1.setDisable(true);
-		region2.setStyle("-fx-background-color: white");
 		text2.setDisable(false);
 	}
 	
 	@FXML protected void checkTest(ActionEvent event) {
-		Region region1 = (Region) text1.lookup(".content");
-		Region region2 = (Region) text2.lookup(".content");
-		
-		region2.setStyle("-fx-background-color: gray");
 		text2.setDisable(true);
-		region1.setStyle("-fx-background-color: white");
 		text1.setDisable(false);
 	}
 }
