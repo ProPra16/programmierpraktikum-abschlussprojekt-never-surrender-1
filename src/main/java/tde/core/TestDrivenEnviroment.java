15a9e2fdd349c2f91040ffc0acad6a786f8c239c
Michael Borisov <michaelborisov9@gmail.com>
2016-07-09 13:09:08 +0200

    GUI switched to TestDrivenEnviroment

--- src/main/java/tde/core/TestDrivenEnviroment.java
+++ src/main/java/tde/core/TestDrivenEnviroment.java
@@ -1,9 +1,14 @@
 package tde.core;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -12,16 +17,43 @@ import javafx.stage.Stage;
  */
 
 public class TestDrivenEnviroment extends Application {
-	@Override
-    public void start(Stage primaryStage) throws Exception{
-		//Hier wird die GUI geladen
-    	Parent root = new GridPane();
-    	primaryStage.setTitle("TestDrivenEnviroment");
-    	primaryStage.setScene(new Scene(root));
-    	primaryStage.show();
+	static boolean path = false;
+	static Stage work = new Stage();
+	static Stage main = new Stage();
+	
+	public static void main(String[] args) {
+        Application.launch((java.lang.String[])null);
     }
-
-	static void init(String[] args){
-		launch(args);
+	
+	public static void load(boolean accept) {
+		path = accept;
+		work.close();
+		main.show();
 	}
+	
+	@Override
+    public void start(Stage stage) {
+		BorderPane page;
+		Pane pane;
+		Scene scene;
+		
+		try {
+			page = (BorderPane) FXMLLoader.load(getClass().getResource("../gui/GUI.fxml"));
+			scene = new Scene(page);
+			main.setScene(scene);
+			main.setTitle("TDDT");
+			main.setMaximized(true);
+			main.hide();
+			
+			pane = FXMLLoader.load(getClass().getResource("../gui/Workspace.fxml"));
+			scene = new Scene(pane);
+			work.setScene(scene);
+			work.setTitle("Workspace");
+			work.show();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+    }
 }
