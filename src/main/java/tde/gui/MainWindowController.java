d14ee44a61c84368dcd2c096a68426207b082b89
Michael Borisov <michaelborisov9@gmail.com>
2016-07-09 13:07:24 +0200

    Controller wird geschrieben

--- src/main/java/tde/gui/MainWindowController.java
+++ src/main/java/tde/gui/MainWindowController.java
@@ -1,8 +1,15 @@
 package tde.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 /**
  * Controller für die eigentliche Benutzeroberfläche
  */
 
 public class MainWindowController {
+	@FXML protected void handleLoadButtonAction(ActionEvent event) {
+		boolean accept = true;
+		GUI.load(accept);
+	}
 }
