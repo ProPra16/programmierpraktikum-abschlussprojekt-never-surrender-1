90589609e00a3ac4191762a1dc67f4e679811d73
alkru108 <alexander.krusch@uni-duesseldorf.de>
2016-07-29 15:28:42 +0200

    Testclass mit XMLparser derm nach Attributen sucht

--- src/main/java/tde/core/Test.java
+++ src/main/java/tde/core/Test.java
@@ -1,7 +1,18 @@
 package tde.core;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import tde.file.XMLParser;
 import vk.core.api.CompilationUnit;
@@ -25,7 +36,7 @@ public class Test {
 	 * 
 	 * 
 	*/
-	public void init(TDEDataStore dataStore) {
+	/*public void init(TDEDataStore dataStore) {
 		String filePath = dataStore.getWorkspace() + TDEDataStore.separator + dataStore.getProjectName();
 
 		File f = new File(filePath);
@@ -55,6 +66,118 @@ public class Test {
 		else System.err.println(String.format("No Files in: %s!", f.getPath()));
 
 
+	}*/
+	
+	public void init(String filePath){
+		
+		File f = new File(filePath);//muss mit dem richtigen Verzeichnis ersetzt werden
+		File[] files = f.listFiles();
+		
+		int n = files.length;
+		String s;
+		int zaehler = 0;
+		
+		CompilationUnit[] classCode = new CompilationUnit[n];
+		CompilationUnit[] test = new CompilationUnit[n];
+		
+		String[] classCodeName = new String[n];
+		String[] testName = new String[n];
+		
+		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder dBuilder = null;
+		try {
+			dBuilder = dbFactory.newDocumentBuilder();
+		} catch (ParserConfigurationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		Document doc = null;
+		
+		NodeList classCodelist;
+		NodeList testlist;
+		
+		Node classCodeNode;
+		Node testNode;
+		
+		Element el;
+		
+		
+		for(int i = 0; i < n; i++){
+			try {
+				doc = dBuilder.parse(files[i]);//lade die xml-Dateien
+			} catch (SAXException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			doc.getDocumentElement().normalize();
+			
+			testlist = doc.getElementsByTagName("test");//sucht nach Stichwoertern
+			classCodelist = doc.getElementsByTagName("class");
+			
+			for(int temp = 0; temp < classCodelist.getLength(); temp++){
+
+				classCodeNode = classCodelist.item(temp);
+				
+				s = null;
+				el = null;
+				
+				if (classCodeNode.getNodeType() == Node.ELEMENT_NODE) {
+					
+					el = (Element) classCodeNode;
+					s = el.getTextContent();//erstellt einen String der alles aus code enthaelt 
+					
+				}
+
+				if(el.getAttribute("name") != null){// id soll class Name sein
+					classCode[i] = new CompilationUnit(el.getAttribute("name"), s, false);//wenn die Klasse existiert wird mit ihr eine CompilationUnit erstellt
+					zaehler++;
+				}
+			}
+			
+			for(int temp = 0; temp < testlist.getLength(); temp++){
+
+				testNode = testlist.item(temp);
+				
+				s = null;
+				el = null;
+				
+				if (testNode.getNodeType() == Node.ELEMENT_NODE) {
+
+					el = (Element) testNode;
+					s = el.getTextContent();//erstellt einen String der alles aus test enthaelt 
+					
+				}
+				
+				if(el.getAttribute("name") != null){
+					test[i] = new CompilationUnit(el.getAttribute("name"), s, true);//wenn der Test existiert wird mit ihr eine CompilationUnit erstellt
+					zaehler++;
+				}
+					
+			}
+			
+		}
+
+		gesamt = new CompilationUnit[zaehler];// erstellt ein neues Array in der groesse der existierenden Klassen und Tests
+		int i = 0;
+		
+		for(int temp = 0; temp < classCode.length; temp++){//befuellt gesamt mit Klassen
+			if(classCode[temp] != null){
+				gesamt[i] = classCode[temp];
+				i++;
+			}
+		}
+		
+		for(int temp = 0; temp < test.length; temp++){//befuellt gesamt mit Tests
+			if(test[temp] != null){
+				gesamt[i] = test[temp];
+				i++;
+			}
+		}
 	}
 
 	/**
